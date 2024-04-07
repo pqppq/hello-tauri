9bfa27d31f553421d97b4e4a9433dc02b06c2752
pqppq <yutatsu0119@gmail.com>
2024-04-07 16:07:21 +0900

    chore: add menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
+use tauri::{CustomMenuItem, Manager, Menu, MenuItem, Submenu};
 
 #[derive(Clone, serde::Serialize)]
 struct Payload {
@@ -15,6 +15,17 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let close = CustomMenuItem::new("close".to_string(), "Close");
+    let submenu = Submenu::new(
+        "File".to_string(),
+        Menu::new().add_item(close).add_item(quit),
+    );
+    let menu = Menu::new()
+        .add_native_item(MenuItem::Copy)
+        .add_item(CustomMenuItem::new("hide", "-"))
+        .add_submenu(submenu);
+
     tauri::Builder::default()
         // only include this code on debug builds
         // .setup(|app| {
@@ -41,6 +52,21 @@ fn main() {
             .unwrap();
             Ok(())
         })
+        .menu(menu)
+            .on_menu_event(|event| {
+                match event.menu_item_id() {
+                    "quit" => {
+                        std::process::exit(0);
+                    }
+                    "close" => {
+                        event.window().close().unwrap();
+                    }
+                    "hide" => {
+                        event.window().hide().unwrap();
+                    }
+                    _ => {}
+                }
+            })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
